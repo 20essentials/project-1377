c709d33b57d2d1be5c376fb7234f6856991f18e0
DominicDev <s0l0estudi4r@gmail.com>
2026-05-11 01:04:36 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/favicon-maker/sw.js', { scope: '/favicon-maker/' })})}
