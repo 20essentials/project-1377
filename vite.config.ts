bee84a7a1927a2ac36286f94ba08816f2bac144b
DominicDev <s0l0estudi4r@gmail.com>
2026-05-11 01:07:22 +0200

    v0.0.6

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,49 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/project-1377/",
+
   plugins: [
     react(),
+
     VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: { enabled: true },
-      includeAssets: ['/fonts/*.woff2'],
+      registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true,
+      },
+
+      includeAssets: ["fonts/*.woff2"],
+
       manifest: {
-        name: 'Favicon Maker',
-        short_name: 'Favicon Maker',
-        description: 'Design and customize your own favicons with ease. Convert images, draw from scratch, or use our advanced tools to give your website a unique touch.',
-        theme_color: '#09090b',
+        name: "Favicon Maker",
+        short_name: "Favicon Maker",
+
+        description:
+          "Design and customize your own favicons with ease. Convert images, draw from scratch, or use our advanced tools to give your website a unique touch.",
+
+        theme_color: "#09090b",
+        background_color: "#09090b",
+
         icons: [
           {
-            src: 'rataTroncha.ico',
-            sizes: '192x192',
-            type: 'image/ico'
+            src: "rataTroncha.ico",
+            sizes: "192x192",
+            type: "image/x-icon",
           },
           {
-            src: 'rataTroncha.ico',
-            sizes: '512x512',
-            type: 'image/ico'
-          }
-        ]
-      }
-    })
+            src: "rataTroncha.ico",
+            sizes: "512x512",
+            type: "image/x-icon",
+          },
+        ],
+      },
+    }),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
